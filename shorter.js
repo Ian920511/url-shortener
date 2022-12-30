76df79f7bf7277bede2f748a5ebf3477544bad60
Ian_Lin <ian920511@gmail.com>
2022-12-30 12:38:44 +0800

    feat: add shorterURL function

--- shorter.js
+++ shorter.js
@@ -1,3 +1,18 @@
-function shorter() {
-  return;
+function sample(collection) {
+  const index = Math.floor(Math.random() * collection.length);
+  return index;
 }
+
+function shorter(times) {
+  const collection =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+  let answer = "";
+  for (let i = 1; i <= times; i++) {
+    answer += collection[sample(collection)];
+  }
+
+  return answer;
+}
+
+module.exports = shorter;
